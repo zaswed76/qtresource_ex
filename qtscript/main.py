5f22753d45387f313c91fc228b4d91e05f5ab6fe
Cassa <zaswed76@gmail.com>
2017-11-09 16:49:15 +0200

    creare project

--- qtscript/main.py
+++ qtscript/main.py
@@ -1,11 +1,15 @@
-
-
+import os
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, uic
+
+ROOT = os.path.join(os.path.dirname(__file__))
+UI_DIR = os.path.join(ROOT, "ui")
 
 class Main(QtWidgets.QLabel):
     def __init__(self):
         super().__init__()
+        self.form = uic.loadUi(
+            os.path.join(UI_DIR, "one.ui"), self)
         self.resize(300, 200)
         self.setText("TEST")
 
