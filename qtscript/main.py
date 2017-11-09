c7f85f20cc1174264ba5f0fd2773610f5ae7235c
Cassa <zaswed76@gmail.com>
2017-11-09 16:36:40 +0200

    creare project

--- qtscript/main.py
+++ qtscript/main.py
@@ -1,9 +1,20 @@
 
 
-class Main:
+import sys
+from PyQt5 import QtWidgets
+
+class Main(QtWidgets.QLabel):
     def __init__(self):
-        print("test")
-        self.a = input(">>>>>>>>>>>")
+        super().__init__()
+        self.resize(300, 200)
+        self.setText("TEST")
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    # app.setStyleSheet(open('./etc/{0}.qss'.format('style'), "r").read())
+    main = Main()
+    main.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    Main()
+    main()
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         install_requires=[],
         entry_points={
             'console_scripts':
-                ['qtscript = qtscript.main:Main']
+                ['qtscript = qtscript.main:main']
         }
 
 )
