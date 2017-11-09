757396ae64b319f20e8751da5367ce39574c0953
Cassa <zaswed76@gmail.com>
2017-11-10 00:19:10 +0200

    creare project

--- qtscript/main.py
+++ qtscript/main.py
@@ -1,17 +1,23 @@
 import os
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtGui
+from  qtscript import images_rc
 
 ROOT = os.path.join(os.path.dirname(__file__))
 UI_DIR = os.path.join(ROOT, "ui")
 
-class Main(QtWidgets.QLabel):
+class Main(QtWidgets.QFrame):
     def __init__(self):
         super().__init__()
-        self.form = uic.loadUi(
-            os.path.join(UI_DIR, "one.ui"), self)
+        # self.box = QtWidgets.QHBoxLayout(self)
+        # self.form = uic.loadUi(
+        #     os.path.join(UI_DIR, "one.ui"), self)
+        # self.box.addWidget(self.form)
+
+        self.lb = QtWidgets.QLabel(self)
+        self.lb.setPixmap(QtGui.QPixmap(':two.png'))
         self.resize(300, 200)
-        self.setText("TEST")
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
